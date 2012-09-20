eae6acd3ae4eb0bfe3eecb51a8f19880f863419b
Jakob Rath <jakob@jakobrath.eu>
2012-09-20 18:09:04 +0200

    Add "Sub Main" functionality: Capture from all devices, wait for Escape key, and then close all devices.

--- iRO Castle Watch Console/Main.vb
+++ iRO Castle Watch Console/Main.vb
@@ -3,13 +3,13 @@ Imports SharpPcap
 
 Module Main
 
-    Sub Main()
+    Public Sub Main()
 
         Console.WriteLine("iRO Castle Watch {0}", Version.VersionString)
         Console.WriteLine("using SharpPcap {0}", SharpPcap.Version.VersionString)
         Console.WriteLine()
 
-        'Retrieve device list
+        ' Retrieve device list
         Dim devices = CaptureDeviceList.Instance
 
         If devices.Count < 1 Then
@@ -21,19 +21,56 @@ Module Main
         Console.WriteLine("----------------------------------------------------")
         Console.WriteLine()
 
+        ' Enumerate devices and display info
         Dim i As Integer = 1
-
         For Each device In devices
             Console.WriteLine("{0}. {1}{3}   {2}", i, device.Name, device.Description, Environment.NewLine)
             i += 1
         Next
 
         Console.WriteLine()
+        Console.WriteLine("This application will capture packets from all devices.")
 
+        ' Start listening to all devices
+        Dim CapturedDevices As New List(Of ICaptureDevice)
+        For Each device In devices
+            AddHandler device.OnPacketArrival, New PacketArrivalEventHandler(AddressOf device_OnPacketArrival)
 
+            device.Open(DeviceMode.Promiscuous, 1000)
 
+            device.Filter = "ip and tcp"
 
-        Console.ReadKey()
+            Console.WriteLine("-- Listening on {0}...", device.Description)
+
+            device.StartCapture()
+            CapturedDevices.Add(device)
+        Next
+
+        Console.WriteLine()
+        Console.WriteLine("Ready. Press [Escape] to exit...")
+
+        Do
+            Dim key = Console.ReadKey
+
+            If key.Key = ConsoleKey.Escape Then
+                Exit Do
+            End If
+        Loop
+
+        Console.WriteLine()
+        Console.WriteLine("Closing devices...")
+
+        For Each device In CapturedDevices
+            Console.WriteLine("-- Closing {0}.", device.Description)
+            device.StopCapture()
+            device.Close()
+        Next
+
+    End Sub
+
+    Private Sub device_OnPacketArrival(sender As Object, e As CaptureEventArgs)
+        Dim device = DirectCast(sender, ICaptureDevice)
+
 
     End Sub
 
