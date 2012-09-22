ae97eee8a78901fdb8d219269041e21483f5767e
Jakob Rath <jakob@jakobrath.eu>
2012-09-23 01:35:58 +0200

    Add check box to tell window to stay on top. Add WoE button click handlers, but I don't know yet how to implement it.

--- iRO Castle Status GUI/MainWindow.xaml.vb
+++ iRO Castle Status GUI/MainWindow.xaml.vb
@@ -1,3 +1,15 @@
 ﻿Class MainWindow 
 
+    Private Sub ShowWoE1_Click(sender As Object, e As RoutedEventArgs) Handles ShowWoE1.Click
+        ShowWoE(WoE.Type.WoE1)
+    End Sub
+
+    Private Sub ShowWoE2_Click(sender As Object, e As RoutedEventArgs) Handles ShowWoE2.Click
+        ShowWoE(WoE.Type.WoE2)
+    End Sub
+
+    Private Sub ShowWoE(Type As WoE.Type)
+        ' ...
+    End Sub
+
 End Class
