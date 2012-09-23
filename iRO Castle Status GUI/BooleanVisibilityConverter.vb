562c697f785fcc75cb725333e11510fe16b5e5d7
Jakob Rath <jakob@jakobrath.eu>
2012-09-23 15:21:50 +0200

    Use BooleanVisibilityConverter to decide which Castles to display.

---
+++ iRO Castle Status GUI/BooleanVisibilityConverter.vb
@@ -0,0 +1,30 @@
+﻿
+<ValueConversion(GetType(Boolean), GetType(Visibility))>
+Public Class BooleanVisibilityConverter
+    Implements IValueConverter
+
+    Public Function Convert(value As Object, targetType As Type, parameter As Object, culture As Globalization.CultureInfo) As Object Implements IValueConverter.Convert
+
+        If TypeOf value Is Boolean Then
+            Dim b = CBool(value)
+
+            Return If(b, Visibility.Visible, Visibility.Collapsed)
+        Else
+            Throw New NotSupportedException("BooleanVisibilityConverter.Convert only supports Boolean as value type.")
+        End If
+
+    End Function
+
+    Public Function ConvertBack(value As Object, targetType As Type, parameter As Object, culture As Globalization.CultureInfo) As Object Implements IValueConverter.ConvertBack
+
+        If TypeOf value Is Visibility Then
+            Dim v = DirectCast(value, Visibility)
+
+            Return (v = Visibility.Visible)
+        Else
+            Throw New NotSupportedException("BooleanVisibilityConverter.Convert only supports System.Windows.Visibility as value type.")
+        End If
+
+    End Function
+
+End Class
