4a6e09e9aa41fe794b181c54209a51542c0cb517
Jakob Rath <jakob@jakobrath.eu>
2012-09-30 15:17:11 +0200

    Remove manifest (again), and update assembly info.

--- iRO Castle Status/My Project/AssemblyInfo.vb
+++ iRO Castle Status/My Project/AssemblyInfo.vb
@@ -11,10 +11,10 @@ Imports System.Windows
 
 ' Review the values of the assembly attributes
 
-<Assembly: AssemblyTitle("iRO_Castle_Status_GUI")> 
+<Assembly: AssemblyTitle("iRO Castle Status")> 
 <Assembly: AssemblyDescription("")> 
 <Assembly: AssemblyCompany("")> 
-<Assembly: AssemblyProduct("iRO_Castle_Status_GUI")> 
+<Assembly: AssemblyProduct("iRO Castle Status")> 
 <Assembly: AssemblyCopyright("Copyright ©  2012")> 
 <Assembly: AssemblyTrademark("")> 
 <Assembly: ComVisible(false)>
@@ -55,5 +55,5 @@ Imports System.Windows
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.0")> 
-<Assembly: AssemblyFileVersion("1.0.0.0")> 
+<Assembly: AssemblyVersion("1.1.0.0")> 
+<Assembly: AssemblyFileVersion("1.1.0.0")> 
