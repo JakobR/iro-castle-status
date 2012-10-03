f7f4572d514a6a6c63937b1366ff609edffa6ec7
Jakob Rath <jakob@jakobrath.eu>
2012-10-03 12:14:45 +0200

    Fix versioning.

--- iRO Castle Status/My Project/AssemblyInfo.vb
+++ iRO Castle Status/My Project/AssemblyInfo.vb
@@ -56,4 +56,4 @@ Imports System.Windows
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
 <Assembly: AssemblyVersion("1.2.*")> 
-<Assembly: AssemblyFileVersion("1.2.*")> 
+'<Assembly: AssemblyFileVersion("1.2.*")> 
